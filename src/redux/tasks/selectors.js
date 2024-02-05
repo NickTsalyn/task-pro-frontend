3f0073b5877d395e53800adaacadc7cdcd88c1a6
YuliaSilk <yuliashevchyk0506@gmail.com>
2024-02-05 23:33:33 +0200

    Create selectors.js
    create selectors task

---
+++ src/redux/tasks/selectors.js
@@ -0,0 +1,13 @@
+// import { createSelector } from "@reduxjs/toolkit";
+
+// export const selectCards = state => state.contacts.cards;
+// export const selectFilter = state => state.filter.value;
+// export const selectIsLoading = state => state.cards.isLoading;
+// export const selectError = state => state.contacts.error;
+
+
+// export const selectVisibleContacts = createSelector (
+//     [selectCards], (contacts, filter) => {
+// return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
+//     }
+// ) 
