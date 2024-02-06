d6e776f8ed8c6575e25149605f82ae736f7a4164
YuliaSilk <yuliashevchyk0506@gmail.com>
2024-02-06 21:35:12 +0200

    Update slice.js
    update tasks slice

--- src/redux/tasks/slice.js
+++ src/redux/tasks/slice.js
@@ -1,16 +1,15 @@
-// import { createSlice } from "@reduxjs/toolkit";
-// import { fetchCards, addCard, deleteCard } from "./operations";
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchTasks, addTask, deleteTask } from "./operations";
 
-// const contactsSlise = createSlice ({
-//     name: "contacts",
-//     initialState: {
-//         contacts: [],
-//         isLoading: false,
-//         error: null,
-//         filter: '',
-//         },
+const tasksSlise = createSlice ({
+    name: "tasks",
+    initialState: {
+        tasks: [],
+        // isLoading: false,
+        error: null,
+        },
        
-//  reducers: {
+ reducers: {
     
 //     setFilter: (state, action) => {
 //         state.filter = action.payload;
@@ -18,50 +17,45 @@
   
 //     },
 
-//     extraReducers: {
-//         [fetchContacts.pending](state) {
-//             state.isLoading = true;
-//         },
-//         [fetchContacts.fulfilled] (state, action) {
-//             state.isLoading = false;
-//             state.error = null;
-//             state.contacts = action.payload;
-//         },
-//         [fetchContacts.rejected] (state, action) {
-//             state.isLoading = false;
-//             state.error = action.payload;
-//         },
-//         [addContact.pending](state) {
-//             state.isLoading = true;
-//         },
-//         [addContact.fulfilled](state, action){
-//             state.isLoading = false;
-//             state.error = null;
-//             state.contacts.push(action.payload);
-//         },
-//         [addContact.rejected](state, action){
-//             state.isLoading = false;
-//             state.error = action.payload;
-//         },
-//         [deleteContact.pending](state){
-//             state.isLoading = true;
-//         },
-//         [deleteContact.fulfilled](state, action){
-//             state.isLoading = false;
-//             state.error = null;
-//             state.contacts = state.contacts.filter((contact) => contact.id !== action.payload.id);
-//             // const index = state.contacts.findIndex(task => task.id === action.payload);
-//             // state.contacts.splice(index, 1);
-//         },
-//         [deleteContact.rejected](state, action){
-//             state.isLoading = false;
-//             state.error = action.payload;
-//         }
+    extraReducers: {
+        [fetchTasks.pending](state) {
+            state.isLoading = true;
+        },
+        [fetchTasks.fulfilled] (state, action) {
+            state.isLoading = false;
+            state.error = null;
+            state.cards = action.payload;
+        },
+        [fetchTasks.rejected] (state, action) {
+            state.isLoading = false;
+            state.error = action.payload;
+        },
+        [addTask.pending](state) {
+            state.isLoading = true;
+        },
+        [addTask.fulfilled](state, action){
+            state.isLoading = false;
+            state.error = null;
+            state.tasks.push(action.payload);
+        },
+        [addTask.rejected](state, action){
+            state.isLoading = false;
+            state.error = action.payload;
+        },
+        [deleteTask.pending](state){
+            state.isLoading = true;
+        },
+        [deleteTask.fulfilled](state, action){
+            state.isLoading = false;
+            state.error = null;
+            state.tasks = state.tasks.filter((task) => task.id !== action.payload.id);
+        },
+        [deleteTask.rejected](state, action){
+            state.isLoading = false;
+            state.error = action.payload;
+        }
 
-//     }
-// })
+    }
+}})
 
-
-
-
-// export const contactsReducer = contactsSlise.reducer;
+export const contactsReducer = tasksSlise.reducer;
