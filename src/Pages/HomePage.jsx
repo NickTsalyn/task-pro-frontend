d9b141e702a661557bcdcbbad8c0d410d5306669
KaterynaSch <128621831+KaterynaSch@users.noreply.github.com>
2024-02-07 19:21:58 +0200

    add-text

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { Header } from 'components/Header/Header';
 import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
+import {  Text, TextA } from './HomePage.styled';
 // import { Loader } from 'components/Loader/Loader';
 
 const HomeWrapper = styled.div`
@@ -13,6 +14,7 @@ export default function HomePage() {
   return (
     <HomeWrapper>
       <Header />
+      <Text>Before starting your project, it is essential <TextA>to create a board</TextA> to visualize and track all the necessary tasks and milestones. This board serves as a powerful tool to organize the workflow and ensure effective collaboration among team members.</Text>
       {/* <Loader/>            */}
       <Suspense fallback={null}>
           <Outlet />
---
+++ src/Pages/HomePage.styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Text = styled.p`
+  margin: 258px 20px 0;  
+  font-size: 12px;
+  line-height: 1.33333;
+  letter-spacing: -0.02em;
+  text-align: center;
+  color: rgba(22, 22, 22, 0.7);
+`;
+
+export const TextA = styled.a`
+    border :none;
+    background-color: inherit;
+    color: #5255bc;
+`;
