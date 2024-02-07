aab44b60de565a9c2b1a51b9d50a99658f321510
NickTsalyn <129570585+NickTsalyn@users.noreply.github.com>
2024-02-07 17:41:10 -0500

    create translation feature

---
+++ src/components/SelectLanguage/SelectLanguage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import { SelectStyled } from './SelectLanguage.styled';
+import { useTranslation } from 'react-i18next';
+import CustomDropdown from 'components/Select/Select';
+
+export const SelectLanguage = () => {
+  const { i18n } = useTranslation('global');
+
+  const handleChange = lang => {
+    i18n.changeLanguage(lang);
+  };
+
+  const handleChangeLanguage = e => {
+    const selectedLanguage = e.target.value;
+    handleChange(selectedLanguage);
+  };
+
+  return (
+      <CustomDropdown handleChangeLanguage={handleChangeLanguage} handleChange={handleChange}/>
+  );
+};
