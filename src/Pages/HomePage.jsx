ed0bb25bdf8be01eb136721652ee4314f3d0a5e7
KaterynaSch <128621831+KaterynaSch@users.noreply.github.com>
2024-02-07 23:46:39 +0200

    fixed-styles

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,7 +2,7 @@ import { Header } from 'components/Header/Header';
 import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
-import {  Text, TextA } from './HomePage.styled';
+import { ButtonText, Text} from './HomePage.styled';
 // import { Loader } from 'components/Loader/Loader';
 
 const HomeWrapper = styled.div`
@@ -14,7 +14,7 @@ export default function HomePage() {
   return (
     <HomeWrapper>
       <Header />
-      <Text>Before starting your project, it is essential <TextA>to create a board</TextA> to visualize and track all the necessary tasks and milestones. This board serves as a powerful tool to organize the workflow and ensure effective collaboration among team members.</Text>
+      <Text>Before starting your project, it is essential<ButtonText> to create a board</ButtonText> to visualize and track all the necessary tasks and milestones. This board serves as a powerful tool to organize the workflow and ensure effective collaboration among team members.</Text>
       {/* <Loader/>            */}
       <Suspense fallback={null}>
           <Outlet />
@@ -22,3 +22,4 @@ export default function HomePage() {
     </HomeWrapper>
   );
 }
+
