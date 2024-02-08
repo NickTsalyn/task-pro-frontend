420b687463fc33b89d4129706576f07d8f6555ba
YuliaSilk <yuliashevchyk0506@gmail.com>
2024-02-08 14:41:47 +0200

    Update selector.js

--- src/redux/tasks/selectors.js
+++ src/redux/tasks/selectors.js
@@ -1,10 +1,10 @@
 // import { createSelector } from "@reduxjs/toolkit";
 
-export const selectTasks = state => state.tasks.tasks;
+export const selectTasks = state => state.tasks.items;
 // export const selectFilter = state => state.filter.value;
 export const selectIsLoading = state => state.tasks.isLoading;
 export const selectError = state => state.tasks.error;
-
+export const getTasks = state => state.tasks.items;
 
 // export const selectVisibleContacts = createSelector (
 //     [selectCards], (cards, filter) => {
