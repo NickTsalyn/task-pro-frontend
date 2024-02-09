f84c46029a3541dbac12f13e00cf10b75bcf554a
NickTsalyn <129570585+NickTsalyn@users.noreply.github.com>
2024-02-08 19:12:19 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,19 +18,19 @@ import './fonts/poppins-v20-latin-regular.woff2';
 import './fonts/poppins-v20-latin-500.woff2';
 import './fonts/poppins-v20-latin-600.woff2';
 
-import global_en from './translations/en/global.json'
-import global_cn from './translations/cn/global.json'
-import global_de from './translations/de/global.json'
-import global_es from './translations/es/global.json'
-import global_fr from './translations/fr/global.json'
-import global_gr from './translations/gr/global.json'
-import global_he from './translations/he/global.json'
-import global_ita from './translations/ita/global.json'
-import global_jp from './translations/jp/global.json'
-import global_pl from './translations/pl/global.json'
-import global_ro from './translations/ro/global.json'
-import global_tr from './translations/tr/global.json'
-import global_ua from './translations/ua/global.json'
+import global_en from './translations/en/global.json';
+import global_cn from './translations/cn/global.json';
+import global_de from './translations/de/global.json';
+import global_es from './translations/es/global.json';
+import global_fr from './translations/fr/global.json';
+import global_gr from './translations/gr/global.json';
+import global_he from './translations/he/global.json';
+import global_ita from './translations/ita/global.json';
+import global_jp from './translations/jp/global.json';
+import global_pl from './translations/pl/global.json';
+import global_ro from './translations/ro/global.json';
+import global_tr from './translations/tr/global.json';
+import global_ua from './translations/ua/global.json';
 
 const theme = {
   colors: {
@@ -44,7 +44,7 @@ const theme = {
     darkBgn: '#151515',
     darkInputBgn: '#1f1f1f',
     shadowColor: 'rgba(22, 22, 22, 0.08)',
-    colorFilterSVG:""
+    colorFilterSVG: '',
   },
   violetColors: {
     //кольори для VioletTheme
@@ -70,65 +70,68 @@ const theme = {
   spacing: value => `${value * 4}px`,
 };
 
-i18next.use(LanguageDetector).use(initReactI18next).init({
-	interpolation: { escapeValue: false},
-	fallbackLng: 'en',
-	resources: {
-		en: {
-			global: global_en
-		},
-		cn: {
-			global: global_cn
-		},
-		de: {
-			global: global_de
-		},
-		es: {
-			global: global_es
-		},
-		fr: {
-			global: global_fr
-		},
-		gr: {
-			global: global_gr
-		},
-		he: {
-			global: global_he
-		},
-		ita: {
-			global: global_ita
-		},
-		jp: {
-			global: global_jp
-		},
-		pl: {
-			global: global_pl
-		},
-		ro: {
-			global: global_ro
-		},
-		tr: {
-			global: global_tr
-		},
-		ua: {
-			global: global_ua
-		}
-	}
-})
+i18next
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    interpolation: { escapeValue: false },
+    fallbackLng: 'en',
+    resources: {
+      en: {
+        global: global_en,
+      },
+      cn: {
+        global: global_cn,
+      },
+      de: {
+        global: global_de,
+      },
+      es: {
+        global: global_es,
+      },
+      fr: {
+        global: global_fr,
+      },
+      gr: {
+        global: global_gr,
+      },
+      he: {
+        global: global_he,
+      },
+      ita: {
+        global: global_ita,
+      },
+      jp: {
+        global: global_jp,
+      },
+      pl: {
+        global: global_pl,
+      },
+      ro: {
+        global: global_ro,
+      },
+      tr: {
+        global: global_tr,
+      },
+      ua: {
+        global: global_ua,
+      },
+    },
+  });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode i18n={i18next}>
-	<I18nextProvider>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <BrowserRouter basename="task-pro-frontend">
-          <ThemeProvider theme={theme}>
-            <Toaster toastOptions={toastStyles} />
-            <App />
-          </ThemeProvider>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-	</I18nextProvider>
+  <React.StrictMode >
+    <I18nextProvider i18n={i18next}>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <BrowserRouter basename="task-pro-frontend">
+            <ThemeProvider theme={theme}>
+              <Toaster toastOptions={toastStyles} />
+              <App />
+            </ThemeProvider>
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </I18nextProvider>
   </React.StrictMode>
 );
