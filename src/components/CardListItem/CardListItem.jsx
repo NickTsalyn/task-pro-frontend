457cc1a64c320877ff1eee56970b9af7a1af805d
YuliaSilk <yuliashevchyk0506@gmail.com>
2024-02-09 21:08:26 +0200

    Update CadrListItem
    creating visible cards with tasks and drawing them in a column

--- src/components/CardListItem/CardListItem.jsx
+++ src/components/CardListItem/CardListItem.jsx
@@ -1,15 +1,24 @@
 import { TaskCard } from "components/TaskCard/TaskCard";
+import { useSelector } from "react-redux";
+import { selectVisibleTasks } from "redux/tasks/selectors";
+
 
 export const CardListItem = () => {
-    // const tasks = [];
+
+  const visibleTasks = useSelector(selectVisibleTasks);
+    //   if (visibleTasks.length === 0) {
+    //       return <p>No tasks to display.</p>;
+    //   }
     return(
         <>
-          {/* {tasks.map(tasks => */}
             <li >
-             {/* key={task.id} */}
-                <TaskCard/>                 
+              {visibleTasks.map(task =>
+                <TaskCard
+                key={task.id}
+                task={task}
+                />
+                )}                                 
             </li>
-          {/* ) }   */}
         </>      
       );
 };
