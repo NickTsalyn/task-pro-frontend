0cf07bca19fd2ce1f73730d31aa519b63156e9ff
NickTsalyn <129570585+NickTsalyn@users.noreply.github.com>
2024-02-09 23:20:13 -0500

    Revert "bugfix columns modal"

--- src/components/ColumnModal/AddColumnModal/AddColumnModal.jsx
+++ src/components/ColumnModal/AddColumnModal/AddColumnModal.jsx
@@ -1,53 +1,31 @@
 import React from 'react';
+import Modal from 'react-modal';
 import sprite from '../../../images/icons.svg';
-import { Main, Section } from '../../Button/AddColumnButton.styled';
 import '../ColumnModal.css';
-import {
-  ModalCloseButton,
-  StyledSvgClose,
-  AddColumnTitle,
-  AddColumnInput,
-  AddColumnBtn,
-  IconWhiteWrap,
-  StyledSvgDarkPlus,
-} from '../ColumnModal.styled';
+import { ModalCloseButton, StyledSvgClose } from '../ColumnModal.styled';
 
-export const AddColumnModal = ({ onCloseModal }) => {
-  const handlerTitleColumn = evt => {
-    evt.preventDefault();
-  };
+// Modal.setAppElement('#root');
 
+export const AddColumnModal = ({ isOpen, onClose, submitButton, children }) => {
   return (
-    <Main>
-      <Section>
-        <div>
-          <AddColumnTitle>Add column</AddColumnTitle>
+    <Modal
+      isOpen={isOpen}
+      overlayClassName={'modal-overlay'}
+      className={'modal-content'}
+      closeTimeoutMS={300}
+      onRequestClose={() => onClose()}
+      ariaHideApp={false}
+    >
 
-          <ModalCloseButton onClick={onCloseModal}>
-            <StyledSvgClose>
-              <use xlinkHref={`${sprite}#icon-x-close`}></use>
-            </StyledSvgClose>
-          </ModalCloseButton>
-
-          <form onSubmit={handlerTitleColumn}>
-            <AddColumnInput
-              type="text"
-              name="title"
-              placeholder="Title"
-              autoFocus
-            />
-
-            <AddColumnBtn type="submit">
-              <IconWhiteWrap>
-                <StyledSvgDarkPlus>
-                  <use xlinkHref={`${sprite}#icon-plus`}></use>
-                </StyledSvgDarkPlus>
-              </IconWhiteWrap>
-              Add
-            </AddColumnBtn>
-          </form>
-        </div>
-      </Section>
-    </Main>
+   <ModalCloseButton onClick={() => onClose()}>
+          <StyledSvgClose>
+            <use xlinkHref={`${sprite}#icon-x-close`}></use>
+          </StyledSvgClose>
+         
+        </ModalCloseButton>
+        {children}
+     
+      </Modal>
+    
   );
 };
