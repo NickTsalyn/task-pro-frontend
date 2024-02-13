06cc50c9369a6f85ab0e68a028724b4cc5d61f73
ailsoya <akalitata53@gmail.com>
2024-02-13 17:55:21 +0200

    background

--- src/Pages/ScreensPage.jsx
+++ src/Pages/ScreensPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from "react-redux"
 import { selectBoardById } from 'redux/boards/selectors';
 import { HeaderDashboard } from 'components/HeaderDashboard/HeaderDashboard';
@@ -8,12 +8,11 @@ import DashBoard from 'components/DashBoard/DashBoard';
 export default function ScreensPage() {
   const board = useSelector(selectBoardById)
 
-  return (    
-      <ScreensPageWrapper back={board.backgroundURL}>
-        <HeaderDashboard />
-        <DashBoard/>
-        {/* {(boards.length === 0 && filteredColumns.length === 0) ? <ColumnList columns={filteredColumns}/> : <AddColumnButton/> } */}
-        
-      </ScreensPageWrapper>    
+  return (
+    <ScreensPageWrapper back={board.backgroundURL}>
+      <HeaderDashboard />
+      <DashBoard/>
+      {/* {(boards.length === 0 && filteredColumns.length === 0) ? <ColumnList columns={filteredColumns}/> : <AddColumnButton/> } */}
+    </ScreensPageWrapper>  
   );
 }
