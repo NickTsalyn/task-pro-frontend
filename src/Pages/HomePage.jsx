6b82cfc46f0a17eecd33858851e93b961a9ddc36
OleksandrTsybulskiy <126475884+OleksandrTsybulskiy@users.noreply.github.com>
2024-02-15 01:56:02 +0200

    Helmets

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,22 +1,23 @@
 import { Header } from 'components/Header/Header';
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Helmet } from 'react-helmet';
 
-import { HeaderWrapper, HomeWrapper, } from './HomePage.styled';
+import { HeaderWrapper, HomeWrapper } from './HomePage.styled';
 import { Sidebar } from 'components/Sidebar/Sidebar';
 import ScreensPage from './ScreensPage';
 
-export default function HomePage() {  
-
+export default function HomePage() {
   const [isWideScreen, setIsWideScreen] = useState(window.innerWidth >= 1440);
-  const { 
+  const {
     // t,
-     i18n } = useTranslation('global');
+    i18n,
+  } = useTranslation('global');
 
   useEffect(() => {
     const dir = i18n.dir(i18n.language);
     document.documentElement.dir = dir;
- }, [i18n, i18n.language]);
+  }, [i18n, i18n.language]);
   useEffect(() => {
     const handleResize = () => {
       setIsWideScreen(window.innerWidth >= 1440);
@@ -29,15 +30,16 @@ export default function HomePage() {
     };
   }, []);
 
-
   return (
     <HomeWrapper>
+      <Helmet>
+        <title>Boards</title>
+      </Helmet>
       {isWideScreen && <Sidebar />}
       <HeaderWrapper>
-        <Header />       
-        <ScreensPage/>
-      </HeaderWrapper>    
-    </HomeWrapper> 
-
+        <Header />
+        <ScreensPage />
+      </HeaderWrapper>
+    </HomeWrapper>
   );
 }
