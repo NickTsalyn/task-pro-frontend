74e02401f9c74cbd212692501524bdd09febda60
RomanChuykov <95050528+RomanChuykov@users.noreply.github.com>
2024-02-14 22:57:11 +0200

    token
    добавив токен у відправку

--- src/redux/auth/needHelpRequest.jsx
+++ src/redux/auth/needHelpRequest.jsx
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import toast from 'react-hot-toast';
 
 axios.defaults.baseURL = 'https://task-pro-backend-a1c2.onrender.com';
 const setAuthHeader = token => {
@@ -7,16 +8,27 @@ const setAuthHeader = token => {
 };
 
 export const sendHelpRequest =  createAsyncThunk(
-    'user/needhelp',
-    async (needHelpData, thunkAPI) => {
-      console.log("data In CAT",needHelpData)
-      try {
-        const res = await axios.post('/api/users/needHelp', needHelpData);
-        setAuthHeader(res.data.token);
-        console.log("result",res.data)
-        return res.data;
-      } catch (error) {
-        console.log("error",error)
+  'user/needhelp',
+  async (needHelpData, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+    if (persistedToken === null) {
+      return thunkAPI.rejectWithValue('User not authenticated');
+    }
+    
+    
+    
+    try {
+      setAuthHeader(persistedToken);
+      console.log("data In CAT",needHelpData);
+      const res = await axios.post('/api/users/needHelp', needHelpData);
+      
+      toast.success(`Request sent successfully!`);
+      console.log("result",res.data)
+      return res.data;
+    } catch (error) {
+      console.log("error",error)
+      toast.error('Error send request ');
         return thunkAPI.rejectWithValue(error.response.data.message);
       }
     }
