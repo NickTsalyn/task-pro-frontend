9cabe643d5d11132e6c3fc285995cc760c95b79e
YuliaSilk <yuliashevchyk0506@gmail.com>
2024-02-15 15:11:36 +0200

    update
    to add datepicker

--- src/components/AddCard/AddCard.js
+++ src/components/AddCard/AddCard.js
@@ -6,7 +6,6 @@ import { format } from 'date-fns';
 // import DatePicker from 'react-date-picker';
 // import { DatePicker } from 'rsuite';
 
-
 import {
   AddCardBtn,
   AddCardButtonSvg,
@@ -31,8 +30,9 @@ import {
   CalendarContainer,
 
   DayText,
-  DatePickerCalendar,
-  BtnOpenCal
+  // DatePickerCalendar,
+  BtnOpenCal,
+  CustomCalendarContainer
   
 } from './AddCard.styled';
 import { useState } from 'react';
@@ -40,18 +40,27 @@ import { CLoseButton } from 'components/EditProfileModal/EditProfileModal.styled
 import { addTask } from 'redux/tasks/operations';
 import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import { DatePickerNew } from 'components/DatePicker/DatePicker';
 // import DatePicker from 'react-date-picker';
 // import { uk } from 'date-fns/locale';
 
 
 export const AddCard = ({ onCloseModal, id }) => {
   const { t } = useTranslation('global');
-   const [startDate, setStartDate] = useState(new Date());
-   const [selectedDate, setselectedDate] = useState(null);
-   const [isCalendarOpen, setIsCalendarOpen] = useState(true);
-   const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
- 
- 
+    // const [fieldValue, setFieldValue] = useState(null);
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const [isCalendarOpen, setIsCalendarOpen] = useState(false);;
+    const [startDate, setStartDate] = useState(false)
+
+    const handleDateChange = (date) => {
+      setSelectedDate(date);
+    };
+  
+    const toOpenCalendar = () => {
+      setIsCalendarOpen(!isCalendarOpen);
+    };
+  
+  
    const dispatch = useDispatch();
    // const formik = useFormik({
    //   initialValues: {
@@ -62,77 +71,32 @@ export const AddCard = ({ onCloseModal, id }) => {
    //   },
     
    // });
- 
    const getFormattedDate = (date) => {
-    //  const today = new Date();
-   
-     if (isToday(date)) {
-       // Якщо дата - сьогодні
-       return `Today, ${format(date, 'MMMM d')}`;
-     }
-   
-     // Якщо дата не сьогодні
-     return format(date, 'MMMM d, yyyy');
-   };
-   
-   const isToday = (date) => {
-     const today = new Date();
-     return (
-       date.getDate() === today.getDate() &&
-       date.getMonth() === today.getMonth() &&
-       date.getFullYear() === today.getFullYear()
-     );
-   };
-   const currentDate = new Date();
- const formattedDate = getFormattedDate(currentDate);
+    const today = new Date();
+
+    if (isToday(date)) {
+      return `Today, ${format(date, 'MMMM d')}`;
+    }
+    return format(date, 'MMMM d, yyyy');
+  };
+
+  const isToday = (date) => {
+    const today = new Date();
+    return (
+      date.getDate() === today.getDate() &&
+      date.getMonth() === today.getMonth() &&
+      date.getFullYear() === today.getFullYear()
+    );
+  };
+
+  const currentDate = new Date();
+  const formattedDate = getFormattedDate(currentDate);
+
  
- const toOpenCalendar = () => {
-   setIsCalendarOpen(!isCalendarOpen);
-
-
- };
-
- const toggleDatePicker = () => {
-  setIsDatePickerOpen((prevState) => !prevState);
-};
+//  const toggleDatePicker = () => {
+//   setIsDatePickerOpen((prevState) => !prevState);
+// };
  
-const CustomDatePickerButton = ({ value, onClick }) => (
-  <BtnOpenCal onClick={onClick}>
-    {value || 'Select Date'}
-  </BtnOpenCal>)
-
-  // const [startDate, setStartDate] = useState(new Date());
-  // const dispatch = useDispatch();
-  // const formik = useFormik({
-  //   initialValues: {
-  //     title: '',
-  //     description: '',
-  //     priority: '',
-  //     deadline: '',
-  //   },
-
-  // });
-
-  //   const saveCard = ()=>{
-
-  //     const newCard = {
-  //       title: formik.values.title,
-  //       description: formik.values.description,
-  //       priority: formik.values.priority,
-  //       deadline: formik.values.deadline,
-  //     };
-  // dispatch(addTask(newCard));
-
-  //   }
-
-  // const isToday = date => {
-  //   const today = new Date();
-  //   return (
-  //     date.getDate() === today.getDate() &&
-  //     date.getMonth() === today.getMonth() &&
-  //     date.getFullYear() === today.getFullYear()
-  //   );
-  // };
 
   const successToaster = () => {
     toast.success('You successfully added card!', {
@@ -247,43 +211,30 @@ const CustomDatePickerButton = ({ value, onClick }) => (
               <AddCardTextCal>
                 {t('screenPage.render.modal.card.deadline')}
               </AddCardTextCal>
+
               <CalendarContainer>
-                  <DayText>{formattedDate}</DayText>
+            <BtnOpenCal
+              type="button"
+              className="sc-gHRYGD jSCLHb"
+              onClick={toOpenCalendar}
+            >
+              <DayText>{formattedDate}</DayText>
+            </BtnOpenCal>
+            {isCalendarOpen && (
+              <CustomCalendarContainer className="custom-calendar-container">
+                <DatePickerNew
+                   selected={selectedDate}
+                   onChange={(date) => {
+                     setSelectedDate(date);
+                     setFieldValue('deadline', getFormattedDate(date));
+                   }}
+                   dateFormat="dd/MM/yyyy"
+                />
+              </CustomCalendarContainer>
+            )}
+          </CalendarContainer>
 
-
-                      <BtnOpenCal type="button" onClick={toggleDatePicker}>
-                        <svg>
-                          <use xlinkHref={`${sprite}#icon-chevron-down1`}></use>
-                        </svg>
-                      </BtnOpenCal>
-
-                      {isDatePickerOpen && (
-        <DatePickerCalendar
-      selected={startDate}
-      onChange={(date) => setStartDate(date)}
-      customInput={<CustomDatePickerButton />}
-      
-      // onChange={(date) => {
-      //   // isCalendarOpen={isCalendarOpen}
-
-      //   setStartDate(date);
-      //   // setselectedDate(date);
-      //   setFieldValue('deadline', date);
-      // }}
-      format="EEEE, MMMM d"
-    />
-                      )}
-                  {/* <DatePickerCalendar
-                    isCalendarOpen={isCalendarOpen}
-                    selected={selectedDate}
-                    onChange={(date) => {
-                      setStartDate(date);
-                      setselectedDate(date);
-                      setFieldValue('deadline', date);
-                    }}
-                    format="EEEE, MMMM d"
-                  /> */}
-</CalendarContainer>
+             
         
             </AddCardContCal>
           </AddCardOptionCont>
