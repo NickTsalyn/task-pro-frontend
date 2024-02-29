4839d25aa7ddc5347ce03747e6f87a201d6e4946
Elena Berezina <farfalina1908@gmail.com>
2024-02-29 15:54:11 +0200

    add custom styles for add column modal and edit column modal

--- src/components/EditColumnButtons/EditColumnButton/EditColumnButton.jsx
+++ src/components/EditColumnButtons/EditColumnButton/EditColumnButton.jsx
@@ -2,10 +2,25 @@ import React, { useState } from 'react';
 import { EditColumnModal } from 'components/ColumnModal/EditColumnModal/EditColumnModal';
 
 import Modal from 'react-modal';
-import '../ColumnModal.css';
 
 import sprite from '../../../images/icons.svg';
 import { EditButton, EditSVG } from './EditColumnButton.styled';
+
+const customStyles = {
+  content: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    padding: '0',
+    border: 'none',
+  },
+};
+
+Modal.setAppElement('#root');
 
 export const EditColumnButton = ({ column }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -30,8 +45,8 @@ export const EditColumnButton = ({ column }) => {
         isOpen={isModalOpen}
         onRequestClose={closeModal}
         overlayClassName={'modal-overlay'}
-        className={'modal-content'}
         closeTimeoutMS={300}
+        style={customStyles}
       >
         <EditColumnModal onCloseModal={closeModal} column={column} />
       </Modal>
